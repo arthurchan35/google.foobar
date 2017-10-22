057a4e4e6ce540468db5cdee7014557b137c0316
arthurchan35 <arthurchan35@gmail.com>
2017-10-22 20:33:24 +0800

    solution to level 2 first chanllenge

--- en_route_salute/solution.java
+++ en_route_salute/solution.java
@@ -1,9 +1,37 @@
-package​ ​com.google.challenges;​ ​
+package com.google.challenges;
 
-public​ ​class​ ​Answer​ ​{​ ​​ ​​ ​
-​ ​​ ​​ ​​ ​public​ ​static​ ​int​ ​answer(String​ ​s)​ ​{​ ​
+public class Answer {
 
-​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​goes​ ​here.
+	public static int answer(String s) {
+		int toLeft = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			if (s.charAt(i) == '<') {
+				toLeft++;
+			}
+		}
 
-​ ​​ ​​ ​​ ​}​ ​
+		int result = 0;
+		int consectiveToRight = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			if (toLeft < 1) {
+				break;
+			}
+
+			if (s.charAt(i) == '<') {
+				result += toLeft * consectiveToRight;
+				consectiveToRight = 0;
+				toLeft--;
+			}
+			else if (s.charAt(i) == '>') {
+				consectiveToRight++;
+			}
+		}
+		return result * 2;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println(answer(">><><"));
+	}
+
 }
